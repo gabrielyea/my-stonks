11060824152664f1ab2198cb5dbb194b0fa862b4
Gabriel <gabriel.gabrielyea@gmail.com>
2021-09-06 16:41:59 -0500

    Add style to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,11 @@
+/* eslint-disable no-unused-vars */
 import { useDispatch, useSelector } from 'react-redux';
+import { BiBitcoin } from 'react-icons/bi';
 import { fetchAllCoins } from '../redux/slices/coinsSlice';
 import Loader from '../components/loader/Loader';
 import ItemsGallery from '../components/gallery/ItemsGallery';
+import { ReactComponent as Logo } from './doge.svg';
+import styles from './homeStyle.module.scss';
 
 const Home = () => {
   const loading = useSelector((state) => state.coins.loading);
@@ -13,13 +17,25 @@ const Home = () => {
   };
 
   return (
-    <Loader
-      loading={loading}
-      callDispatch={callDispatch}
-      list={list}
-    >
-      <ItemsGallery />
-    </Loader>
+    <div className={styles.mainContainer}>
+
+      <header className={styles.header}>
+        <Logo className={styles.icon} />
+        <div>
+          <h1>Cryptocurrencies</h1>
+        </div>
+      </header>
+      <div className={styles.sectionTitle}>
+        <p>All crypto currencies</p>
+      </div>
+      <Loader
+        loading={loading}
+        callDispatch={callDispatch}
+        list={list}
+      >
+        <ItemsGallery />
+      </Loader>
+    </div>
   );
 };
 export default Home;
