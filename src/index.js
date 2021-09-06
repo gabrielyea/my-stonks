15f407459f734ac52250715c0b0bd93e33e378ec
Gabriel <gabriel.gabrielyea@gmail.com>
2021-09-06 16:40:53 -0500

    Add reset style

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-// import './utils/_resets.scss';
+import './utils/_resets.scss';
 
 ReactDOM.render(
   <React.StrictMode>
