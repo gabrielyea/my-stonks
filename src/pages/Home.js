7f37d2a16971fff4535640c7ea7bd7a21981ff40
Gabriel <gabriel.gabrielyea@gmail.com>
2021-09-07 08:59:58 -0500

    Add transition to home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,24 +2,42 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { BiBitcoin } from 'react-icons/bi';
 import { useState } from 'react';
+import { motion } from 'framer-motion';
 import { fetchAllCoins } from '../redux/slices/coinsSlice';
 import Loader from '../components/loader/Loader';
 import { ReactComponent as Logo } from './doge.svg';
 import styles from './homeStyle.module.scss';
 import ItemsGallery from '../components/gallery/ItemsGallery';
 
+const container = {
+  initial: {
+    opacity: 0,
+  },
+  animate: {
+    opacity: 1,
+  },
+  exit: {
+    opacity: 0,
+  },
+};
+
 const Home = () => {
   const loading = useSelector((state) => state.coins.loading);
   const list = useSelector((state) => state.coins.entities);
   const dispatch = useDispatch();
-  const [selected, setSelected] = useState(null);
 
   const callDispatch = () => {
     dispatch(fetchAllCoins());
   };
 
   return (
-    <div className={styles.mainContainer}>
+    <motion.div
+      variants={container}
+      initial="initial"
+      animate="animate"
+      exit="exit"
+      className={styles.mainContainer}
+    >
 
       <header className={styles.header}>
         <Logo className={styles.icon} />
@@ -37,7 +55,7 @@ const Home = () => {
       >
         <ItemsGallery />
       </Loader>
-    </div>
+    </motion.div>
   );
 };
 export default Home;
