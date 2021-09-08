f1aa2e5ee54f8f83b73389dc47585f88d5a4d76c
Gabriel <gabriel.gabrielyea@gmail.com>
2021-09-08 14:26:30 -0500

    Update slices

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
 import coins from './slices/coinsSlice';
+import history from './slices/historySlice';
 
 export default configureStore({
   reducer: {
     coins,
+    history,
   },
   middleware: [thunk],
 });
