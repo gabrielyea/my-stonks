577dcbebf611ed887996dce0f9c380ed472c5ed3
Gabriel <gabriel.gabrielyea@gmail.com>
2021-09-09 13:50:28 -0500

    Add themes

---
+++ src/utils/themes.js
@@ -0,0 +1,20 @@
+const themes = {
+  pink: {
+    mainColor1: '#f94e90',
+    mainColor2: '#ec4c8a',
+    mainColor3: '#ce4176',
+    mainColor4: '#992e58',
+    mainText: 'white',
+    darkText: '#2d4573',
+  },
+  blue: {
+    mainColor1: '#5687e3',
+    mainColor2: '#3f62a4',
+    mainColor3: '#4167AE',
+    mainColor4: '#2d4573',
+    mainText: 'white',
+    darkText: '#2d4573',
+  },
+};
+
+export default themes;
